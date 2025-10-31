2c232cb6c891efafe150cf00d9abd2d8c1d9dbc6
정민우 소프트웍스 <sae1013@gmail.com>
2025-10-31 12:57:52 +0900

    [REFACTOR] 컨트롤러 폴더 이동

--- src/main/java/com/softworks/joongworld/db/DatabaseHealthController.java
+++ src/main/java/com/softworks/joongworld/db/DatabaseHealthController.java
@@ -1,11 +1,10 @@
-package com.softworks.joongworld.global.health;
+package com.softworks.joongworld.db;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Map;
 
 @RestController
