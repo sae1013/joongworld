0cb7112dc7a17ff7140d15c67fa89a168d8753b6
정민우 소프트웍스 <sae1013@gmail.com>
2025-10-31 17:27:33 +0900

    [FEAT] 글작성하기 페이지 추가

--- src/main/java/com/softworks/joongworld/product/controller/ProductController.java
+++ src/main/java/com/softworks/joongworld/product/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.softworks.joongworld.product.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -15,6 +16,11 @@ public class ProductController {
         this.productService = productService;
     }
 
+    /**
+     * 상품 게시글 조회
+     * @param productId 상품 ID
+     * @return View
+     */
     @GetMapping("/product/{productId}")
     public ModelAndView detail(@PathVariable Long productId) {
         ModelAndView mav = new ModelAndView("product/detail");
@@ -22,8 +28,19 @@ public class ProductController {
         return mav;
     }
 
-    @GetMapping("/posts/{productId}")
-    public String redirectFromLegacyPost(@PathVariable Long productId) {
-        return "redirect:/product/" + productId;
+    /**
+     * 판매글 작성하기
+     * @return View
+     */
+    @GetMapping("/product/new")
+    public ModelAndView productNew(){
+        ModelAndView mav = new ModelAndView("product/new");
+        return mav;
     }
+//    @GetMapping("/product/form")
+//    public ModelAndView form(@RequestParam(name = "type", defaultValue = "regist") String type) {
+//        ModelAndView mav = new ModelAndView("product/form");
+//        mav.addObject("type", type);
+//        return mav;
+//    }
 }
