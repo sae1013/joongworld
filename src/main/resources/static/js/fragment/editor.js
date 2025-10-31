91680b8d5af4dd9019523dd85b45538b79327d74
정민우 소프트웍스 <sae1013@gmail.com>
2025-10-31 15:33:55 +0900

    [FEAT] CK Editor 삽입

---
+++ src/main/resources/static/js/fragment/editor.js
@@ -0,0 +1,245 @@
+/**
+ * This configuration was generated using the CKEditor 5 Builder. You can modify it anytime using this link:
+ * https://ckeditor.com/ckeditor-5/builder/#installation/NoNgNARATAdA7DAHBSBGADFVBWVq4AsccqAnCFAMwgEEhyLo1xNQj2VynbbqogoIAawD2KdGGCowE2TLCoAupHSU6iRADMIioA==
+ */
+
+const {
+    ClassicEditor,
+    Autosave,
+    Essentials,
+    Paragraph,
+    MediaEmbed,
+    PasteFromOffice,
+    ImageUtils,
+    ImageEditing,
+    Mention,
+    Bold,
+    Italic,
+    Underline,
+    Strikethrough,
+    Subscript,
+    Superscript,
+    FontBackgroundColor,
+    FontColor,
+    FontFamily,
+    FontSize,
+    Highlight,
+    Heading,
+    Link,
+    AutoLink,
+    BlockQuote,
+    HorizontalLine,
+    Indent,
+    IndentBlock,
+    Alignment,
+    GeneralHtmlSupport,
+    HtmlComment,
+    List
+} = window.CKEDITOR;
+
+const LICENSE_KEY =
+    'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3NjMxNjQ3OTksImp0aSI6ImU4YWY5NDkxLTAyNmUtNDZiOS04OTJiLWU5MmVjODNjZGExOSIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiLCJzaCJdLCJ3aGl0ZUxhYmVsIjp0cnVlLCJsaWNlbnNlVHlwZSI6InRyaWFsIiwiZmVhdHVyZXMiOlsiKiJdLCJ2YyI6ImJiYWM2ZTExIn0._mWs3ak1ET-okuaLjd1rOPeG3Eat86PBg7kfUDjRDc6nStZz2uFW7bTJZb-u7xwXDcEhW3OFSlajcKQVBD589A';
+
+const editorConfig = {
+    toolbar: {
+        items: [
+            'undo',
+            'redo',
+            '|',
+            'heading',
+            '|',
+            'fontSize',
+            'fontFamily',
+            'fontColor',
+            'fontBackgroundColor',
+            '|',
+            'bold',
+            'italic',
+            'underline',
+            'strikethrough',
+            'subscript',
+            'superscript',
+            '|',
+            'horizontalLine',
+            'link',
+            'mediaEmbed',
+            'highlight',
+            'blockQuote',
+            '|',
+            'alignment',
+            '|',
+            'bulletedList',
+            'numberedList',
+            'outdent',
+            'indent'
+        ],
+        shouldNotGroupWhenFull: true
+    },
+    plugins: [
+        Alignment,
+        AutoLink,
+        Autosave,
+        BlockQuote,
+        Bold,
+        Essentials,
+        FontBackgroundColor,
+        FontColor,
+        FontFamily,
+        FontSize,
+        GeneralHtmlSupport,
+        Heading,
+        Highlight,
+        HorizontalLine,
+        HtmlComment,
+        ImageEditing,
+        ImageUtils,
+        Indent,
+        IndentBlock,
+        Italic,
+        Link,
+        List,
+        MediaEmbed,
+        Mention,
+        Paragraph,
+        PasteFromOffice,
+        Strikethrough,
+        Subscript,
+        Superscript,
+        Underline
+    ],
+    fontFamily: {
+        supportAllValues: true
+    },
+    fontSize: {
+        options: [10, 12, 14, 'default', 18, 20, 22],
+        supportAllValues: true
+    },
+    heading: {
+        options: [
+            {
+                model: 'paragraph',
+                title: 'Paragraph',
+                class: 'ck-heading_paragraph'
+            },
+            {
+                model: 'heading1',
+                view: 'h1',
+                title: 'Heading 1',
+                class: 'ck-heading_heading1'
+            },
+            {
+                model: 'heading2',
+                view: 'h2',
+                title: 'Heading 2',
+                class: 'ck-heading_heading2'
+            },
+            {
+                model: 'heading3',
+                view: 'h3',
+                title: 'Heading 3',
+                class: 'ck-heading_heading3'
+            },
+            {
+                model: 'heading4',
+                view: 'h4',
+                title: 'Heading 4',
+                class: 'ck-heading_heading4'
+            },
+            {
+                model: 'heading5',
+                view: 'h5',
+                title: 'Heading 5',
+                class: 'ck-heading_heading5'
+            },
+            {
+                model: 'heading6',
+                view: 'h6',
+                title: 'Heading 6',
+                class: 'ck-heading_heading6'
+            }
+        ]
+    },
+    htmlSupport: {
+        allow: [
+            {
+                name: /^.*$/,
+                styles: true,
+                attributes: true,
+                classes: true
+            }
+        ]
+    },
+    initialData:
+        '<h2>Congratulations on setting up CKEditor 5! 🎉</h2>\n<p>\n\tYou\'ve successfully created a CKEditor 5 project. This powerful text editor\n\twill enhance your application, enabling rich text editing capabilities that\n\tare customizable and easy to use.\n</p>\n<h3>What\'s next?</h3>\n<ol>\n\t<li>\n\t\t<strong>Integrate into your app</strong>: time to bring the editing into\n\t\tyour application. Take the code you created and add to your application.\n\t</li>\n\t<li>\n\t\t<strong>Explore features:</strong> Experiment with different plugins and\n\t\ttoolbar options to discover what works best for your needs.\n\t</li>\n\t<li>\n\t\t<strong>Customize your editor:</strong> Tailor the editor\'s\n\t\tconfiguration to match your application\'s style and requirements. Or\n\t\teven write your plugin!\n\t</li>\n</ol>\n<p>\n\tKeep experimenting, and don\'t hesitate to push the boundaries of what you\n\tcan achieve with CKEditor 5. Your feedback is invaluable to us as we strive\n\tto improve and evolve. Happy editing!\n</p>\n<h3>Helpful resources</h3>\n<ul>\n\t<li>📝 <a href="https://portal.ckeditor.com/checkout?plan=free">Trial sign up</a>,</li>\n\t<li>📕 <a href="https://ckeditor.com/docs/ckeditor5/latest/installation/index.html">Documentation</a>,</li>\n\t<li>⭐️ <a href="https://github.com/ckeditor/ckeditor5">GitHub</a> (star us if you can!),</li>\n\t<li>🏠 <a href="https://ckeditor.com">CKEditor Homepage</a>,</li>\n\t<li>🧑‍💻 <a href="https://ckeditor.com/ckeditor-5/demo/">CKEditor 5 Demos</a>,</li>\n</ul>\n<h3>Need help?</h3>\n<p>\n\tSee this text, but the editor is not starting up? Check the browser\'s\n\tconsole for clues and guidance. It may be related to an incorrect license\n\tkey if you use premium features or another feature-related requirement. If\n\tyou cannot make it work, file a GitHub issue, and we will help as soon as\n\tpossible!\n</p>\n',
+    language: 'ko',
+    licenseKey: LICENSE_KEY,
+    link: {
+        addTargetToExternalLinks: true,
+        defaultProtocol: 'https://',
+        decorators: {
+            toggleDownloadable: {
+                mode: 'manual',
+                label: 'Downloadable',
+                attributes: {
+                    download: 'file'
+                }
+            }
+        }
+    },
+    mention: {
+        feeds: [
+            {
+                marker: '@',
+                feed: [
+                    /* See: https://ckeditor.com/docs/ckeditor5/latest/features/mentions.html */
+                ]
+            }
+        ]
+    },
+    placeholder: 'Type or paste your content here!'
+};
+
+configUpdateAlert(editorConfig);
+
+ClassicEditor.create(document.querySelector('#editor'), editorConfig);
+
+/**
+ * This function exists to remind you to update the config needed for premium features.
+ * The function can be safely removed. Make sure to also remove call to this function when doing so.
+ */
+function configUpdateAlert(config) {
+    if (configUpdateAlert.configUpdateAlertShown) {
+        return;
+    }
+
+    const isModifiedByUser = (currentValue, forbiddenValue) => {
+        if (currentValue === forbiddenValue) {
+            return false;
+        }
+
+        if (currentValue === undefined) {
+            return false;
+        }
+
+        return true;
+    };
+
+    const valuesToUpdate = [];
+
+    configUpdateAlert.configUpdateAlertShown = true;
+
+    if (!isModifiedByUser(config.licenseKey, '<YOUR_LICENSE_KEY>')) {
+        valuesToUpdate.push('LICENSE_KEY');
+    }
+
+    if (valuesToUpdate.length) {
+        window.alert(
+            [
+                'Please update the following values in your editor config',
+                'to receive full access to Premium Features:',
+                '',
+                ...valuesToUpdate.map(value => ` - ${value}`)
+            ].join('\n')
+        );
+    }
+}
