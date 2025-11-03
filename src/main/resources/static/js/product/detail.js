f4a5d37e5f4f63b97b548dd33e47b6a5a6d9d211
정민우 소프트웍스 <sae1013@gmail.com>
2025-11-03 12:16:13 +0900

    글쓰기 기능 작업 완료

--- src/main/resources/static/js/product/detail.js
+++ src/main/resources/static/js/product/detail.js
@@ -2,6 +2,17 @@
 
 $(function () {
     const $main = $('#mainImage');
+    const FILE_PREFIX = '/files/';
+
+    function resolveImageUrl(path) {
+        if (!path) return '';
+        // https 주소는 그대로 사용
+        if (/^https?:\/\//i.test(path)) {
+            return path;
+        }
+        // 그외 로컬 이미지 경로
+        return FILE_PREFIX + path.replace(/^\/+/, '');
+    }
 
     if ($main.length) {
         const $thumbs = $('.thumbs .thumb');
@@ -13,10 +24,12 @@ $(function () {
 
             const direct = $thumb.data('src');
             const bg = $thumb.css('background-image');
-            let url = direct;
+            let url = resolveImageUrl(direct);
 
             if (url) {
                 $main.css('background-image', `url('${url}')`);
+            } else if (bg && bg !== 'none') {
+                $main.css('background-image', bg);
             }
         });
     }
